61116cd32ee782fcaabfe4a2e6711feb1b331d99
Ritik Jadhav <ritiksjadhav99@gmail.com>
2024-05-31 23:52:50 +0530

    Signin page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" />
+        <Route path="/signin" element={<Signin />} />
         <Route path="/dashboard" />
         <Route path="/send" />
       </Routes>
---
+++ frontend/src/pages/Signin.jsx
@@ -0,0 +1,30 @@
+import BottomWarning from "../components/BottonWarning";
+import Button from "../components/Button";
+import Heading from "../components/Heading";
+import InputBox from "../components/InputBox";
+import SubHeading from "../components/SubHeading";
+
+const Signin = () => {
+  return (
+    <div className="bg-slate-300 h-screen flex justify-center">
+      <div className="flex flex-col justify-center">
+        <div className="rounded-lg bg-white w-80 text-center h-max p-2 px-4">
+          <Heading label={"Sign In"} />
+          <SubHeading label={"Enter your credentials to access your account"} />
+          <InputBox label={"Email"} placeholder={"ritik@gmail.com"} />
+          <InputBox label={"Password"} placeholder={"12345"} />
+          <div className="pt-4">
+            <Button label={"Sign In"} />
+          </div>
+          <BottomWarning
+            label={"Don't have an account?"}
+            linkText={"Sign Up"}
+            to={"/signup"}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signin;
